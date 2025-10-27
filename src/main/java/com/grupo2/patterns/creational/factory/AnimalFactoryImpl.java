c1e17e70306e92619dfbb2d2d0baeafc65832990
Jerov159 <jeronimovallejoquintero@gmail.com>
2025-10-26 22:30:35 -0500

    Se añadieron las propiedades a las entidades Animales, y se empezó a trabajar en el factory method

--- src/main/java/com/grupo2/patterns/creational/factory/AnimalFactoryImpl.java
+++ src/main/java/com/grupo2/patterns/creational/factory/AnimalFactoryImpl.java
@@ -1,4 +1,9 @@
 package com.grupo2.patterns.creational.factory;
 
+import com.grupo2.entity.animales.Animal;
+import com.grupo2.entity.animales.Vaca;
+import com.grupo2.entity.animales.Cerdo;
+import com.grupo2.entity.animales.Gallina;
+
 public class AnimalFactoryImpl {
 }
