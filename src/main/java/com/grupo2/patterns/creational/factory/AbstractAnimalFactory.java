181e719a4fa799cdaf12269a040233de6d2bd01b
Jerov159 <jeronimovallejoquintero@gmail.com>
2025-10-26 22:50:50 -0500

    Fix

--- src/main/java/com/grupo2/patterns/creational/factory/AbstractAnimalFactory.java
+++ src/main/java/com/grupo2/patterns/creational/factory/AbstractAnimalFactory.java
@@ -1,9 +1,4 @@
 package com.grupo2.patterns.creational.factory;
 
-import com.grupo2.entity.animales.Animal;
-
-public interface AbstractAnimalFactory {
-    Animal crearAnimal(String id, double peso, int edad);
-    String crearAlimento();
-    String crearEntorno();
+public class AbstractAnimalFactory {
 }
