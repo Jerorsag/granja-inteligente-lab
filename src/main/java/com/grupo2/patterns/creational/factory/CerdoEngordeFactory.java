65a10427481212f2d267c6c3188241a3002ae8f3
Jerov159 <jeronimovallejoquintero@gmail.com>
2025-10-27 18:01:09 -0500

    Se implementa Factory-method para los distintos casos en los animales de la granja

--- src/main/java/com/grupo2/patterns/creational/factory/CerdoEngordeFactory.java
+++ src/main/java/com/grupo2/patterns/creational/factory/CerdoEngordeFactory.java
@@ -1,4 +1,21 @@
 package com.grupo2.patterns.creational.factory;
 
-public class CerdoEngordeFactory {
+import com.grupo2.entity.animales.Animal;
+import com.grupo2.entity.animales.Cerdo;
+
+public class CerdoEngordeFactory implements AbstractAnimalFactory {
+    @Override
+    public Animal crearAnimal(String id, double peso, int edad) {
+        return new Cerdo(id, "Landrace", peso, edad, peso * 0.80);
+    }
+
+    @Override
+    public String crearAlimento() {
+        return "Concentrado para Engorde Alto en Proteínas";
+    }
+
+    @Override
+    public String crearEntorno() {
+        return "Corral de engorde con temperatura controlada";
+    }
 }
